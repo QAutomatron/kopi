6d74e9d4fb1dcc2e1436d8a9a0ab12545927c2ba
Evgenii Skiridov <evgenii.skiridov@bolt.eu>
2021-12-10 18:59:13 +0200

    Update deps, redo tests

--- sample/src/androidTest/kotlin/com/qautomatron/kopi/sample/ElementActionTest.kt
+++ sample/src/androidTest/kotlin/com/qautomatron/kopi/sample/ElementActionTest.kt
@@ -1,31 +1,17 @@
 package com.qautomatron.kopi.sample
 
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.rule.ActivityTestRule
 import com.qautomatron.kopi.library.element.Element
 import com.qautomatron.kopi.library.resourceToString
 import org.junit.Assert.assertEquals
-import org.junit.Rule
 import org.junit.Test
 
 /**
  * Check for ElementAction
  */
-class ElementActionTest {
-
-    @Rule
-    @JvmField
-    val activityRule = ActivityTestRule(MainActivity::class.java)
+class ElementActionTest : BaseTest() {
 
     private val element = Element(withId(R.id.message_text))
-
-    @Test
-    fun test() {
-        val button = Element(withId(R.id.button_hide))
-        button.tap()
-
-        button.tap()
-    }
 
     @Test
     fun should_get_text_action_when_use_action() {
--- sample/src/androidTest/kotlin/com/qautomatron/kopi/sample/ElementMatcherTest.kt
+++ sample/src/androidTest/kotlin/com/qautomatron/kopi/sample/ElementMatcherTest.kt
@@ -1,20 +1,14 @@
 package com.qautomatron.kopi.sample
 
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.rule.ActivityTestRule
 import com.qautomatron.kopi.library.element.Element
 import com.qautomatron.kopi.library.matcher.first
-import org.junit.Rule
 import org.junit.Test
 
 /**
  * Check for ElementAction
  */
-class ElementMatcherTest {
-
-    @Rule
-    @JvmField
-    val activityRule = ActivityTestRule(MainActivity::class.java)
+class ElementMatcherTest: BaseTest() {
 
     private val element = Element(first(withId(R.id.sameId1)))
 
