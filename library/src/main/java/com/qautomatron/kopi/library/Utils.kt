c9e9971bc51fafdd92686d5d33d5542a0a4661d8
Evgenii Skiridov <evgenii.skiridov@bolt.eu>
2022-06-09 11:19:38 +0300

    more DeviceSteps

--- library/src/main/java/com/qautomatron/kopi/library/Utils.kt
+++ library/src/main/java/com/qautomatron/kopi/library/Utils.kt
@@ -3,10 +3,12 @@ package com.qautomatron.kopi.library
 import android.app.Instrumentation
 import android.content.Context
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.uiautomator.UiDevice
 
 val instrumentation: Instrumentation get() = InstrumentationRegistry.getInstrumentation()
 val targetContext: Context get() = instrumentation.targetContext
 val appContext: Context get() = targetContext.applicationContext
+val mDevice: UiDevice get() = UiDevice.getInstance(instrumentation)
 
 /**
  * Return string from resources
--- library/src/main/java/com/qautomatron/kopi/library/steps/DeviceSteps.kt
+++ library/src/main/java/com/qautomatron/kopi/library/steps/DeviceSteps.kt
@@ -2,12 +2,15 @@ package com.qautomatron.kopi.library.steps
 
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.NoActivityResumedException
 import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.uiautomator.By
 import androidx.test.uiautomator.UiDevice
 import androidx.test.uiautomator.Until
 import com.qautomatron.kopi.library.element.action.SleepAction
 import com.qautomatron.kopi.library.instrumentation
+import com.qautomatron.kopi.library.mDevice
 import com.qautomatron.kopi.library.wait.WaitForActivity
 import com.qautomatron.kopi.library.watcher.Watcher
 import org.junit.Assert.assertTrue
@@ -20,23 +23,12 @@ object DeviceSteps {
     /**
      * Wait for activity by name
      */
-    fun waitForActivity(fullName: String, timeoutInMillis: Int? = null, pollingInMillis: Int? = null) =
+    fun waitForActivity(
+        fullName: String,
+        timeoutInMillis: Int? = null,
+        pollingInMillis: Int? = null
+    ) =
         Watcher.waitForCondition(WaitForActivity(fullName), timeoutInMillis, pollingInMillis)
-
-    /**
-     * Will use UI Automator to wait for package with name to be active
-     */
-    fun packageShouldBeOpened(packageTitle: String, timeoutInSec: Int = 5) {
-        val mDevice = UiDevice.getInstance(instrumentation)
-
-        assertTrue(
-            "Package <$packageTitle> should be present on screen",
-            mDevice.wait(
-                Until.hasObject(By.pkg(packageTitle).depth(0)),
-                (timeoutInSec * 1000).toLong()
-            )
-        )
-    }
 
     /**
      *  Will pause test execution for defined second
@@ -54,4 +46,35 @@ object DeviceSteps {
      * Press Back
      */
     fun pressBack() = Espresso.pressBack()
+
+    /**
+     * Will use UI Automator to wait for package with name to be active
+     */
+    fun packageShouldBeOpened(packageTitle: String, timeoutInSec: Int = 5) {
+        assertTrue(
+            "Package <$packageTitle> should be present on screen",
+            mDevice.wait(
+                Until.hasObject(By.pkg(packageTitle).depth(0)),
+                (timeoutInSec * 1000).toLong()
+            )
+        )
+    }
+
+    /**
+     * Press the home button with uiautomator to minimize previous apps and don't
+     * allow them to interrupt with the test app
+     */
+    fun pressHome() {
+        try {
+            mDevice.pressHome()
+        } catch (ex: NoActivityResumedException) {
+        }
+    }
+
+    /**
+     * Wait for idle state
+     */
+    fun waitForIdle(timeoutInMills: Long = 10000) {
+        mDevice.waitForIdle(timeoutInMills)
+    }
 }
